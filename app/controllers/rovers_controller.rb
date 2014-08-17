b1db11ed86ea618ff7273efbec562c9a90abecc4
Vitalik <ananichev.v.v@gmail.com>
2014-08-17 15:52:11 +0300

    Refactore calculate action

--- app/controllers/rovers_controller.rb
+++ app/controllers/rovers_controller.rb
@@ -10,62 +10,29 @@ class RoversController < ApplicationController
   def calculate
   	@coords = params[:coords].split
     @actions = params[:actions].split(/d*/)
+  	@coords = @coords[0, 2].map(&:to_i) << @coords[-1]
   	
-    @coords = @coords[0, 2].map(&:to_i) << @coords[-1]
-    
-  	@actions.each do |a|
-      if @coords[-1] == "N"
-  	    if a == "M"
-  	      @coords[1] += 1
-  	    elsif a == "L"
-  	      l = NEWS.index(@coords[-1]) - 1
-  	      p @coords[-1] = NEWS[l]
-  	    elsif a == "R"
-  	      r = NEWS.index(@coords[-1]) + 1
-  	      @coords[-1] = NEWS[r]
-        end
-        next
-      end
-  
-      if @coords[-1] == "E"
-  	    if a == "M"
-  	      @coords[0] += 1
-  	    elsif a == "L"
-  	      l = NEWS.index(@coords[-1]) - 1
-  	      @coords[-1] = NEWS[l]
-  	    elsif a == "R"
-  	      r = NEWS.index(@coords[-1]) + 1
-  	      @coords[-1] = NEWS[r]
-        end
-        next
-      end
-  
-      if @coords[-1] == "S"
-  	    if a == "M"
-  	      @coords[1] -= 1
-  	    elsif a == "L"
-  	      l = NEWS.index(@coords[-1]) - 1
-  	      @coords[-1] = NEWS[l]
-  	    elsif a == "R"
-  	      r = NEWS.index(@coords[-1]) + 1
-  	      @coords[-1] = NEWS[r]
-        end
-        next
-      end
-  
-      if @coords[-1] == "W"
-  	    if a == "M"
-  	      @coords[0] -= 1
-  	    elsif a == "L"
-  	      l = NEWS.index(@coords[-1]) - 1
-  	      @coords[-1] = NEWS[l]
-  	    elsif a == "R"
+    @actions.each do |a|
+      if a == "M"
+  	    @coords[1] += 1 if @coords[-1] == "N"
+  	    @coords[0] += 1 if @coords[-1] == "E"
+  	    @coords[1] -= 1 if @coords[-1] == "S"
+  	    @coords[0] -= 1 if @coords[-1] == "W"
+  	  elsif a == "L"
+  	    l = NEWS.index(@coords[-1]) - 1
+  	    @coords[-1] = NEWS[l]
+  	  elsif a == "R"
+  	    r = NEWS.index(@coords[-1]) + 1
+  	    if @coords[-1] == "W"
   	      @coords[-1] = NEWS[0]
-        end
-        next
+  	    else 
+  	      @coords[-1] = NEWS[r]
+  	    end
       end
+      next
     end
-  	p session[:coords] = @coords
+    
+  	session[:coords] = @coords
   	redirect_to root_url
   end
 	
