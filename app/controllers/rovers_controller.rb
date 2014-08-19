e62dbae690ec3186325d9e39372f5464e7df3f1e
ananichev <va@anahoret.com>
2014-08-19 10:50:55 +0300

    Some changes in rovers controller

--- app/controllers/rovers_controller.rb
+++ app/controllers/rovers_controller.rb
@@ -9,27 +9,20 @@ class RoversController < ApplicationController
   
   def calculate
     @coords = params[:coords].split
-    @actions = params[:actions].split(/d*/)
+    @actions = params[:actions].split(/\d*/)
     @coords = @coords[0, 2].map(&:to_i) << @coords[-1]
   	
     @actions.each do |a|
       if a == "M"
         @coords[1] += 1 if @coords[-1] == "N"
-  	@coords[0] += 1 if @coords[-1] == "E"
+  	    @coords[0] += 1 if @coords[-1] == "E"
         @coords[1] -= 1 if @coords[-1] == "S"
-  	@coords[0] -= 1 if @coords[-1] == "W"
+  	    @coords[0] -= 1 if @coords[-1] == "W"
       elsif a == "L"
-  	l = NEWS.index(@coords[-1]) - 1
-  	@coords[-1] = NEWS[l]
+  	    @coords[-1] = NEWS[NEWS.index(@coords[-1]) - 1]
       elsif a == "R"
-  	r = NEWS.index(@coords[-1]) + 1
-  	if @coords[-1] == "W"
-  	  @coords[-1] = NEWS[0]
-  	else 
-  	  @coords[-1] = NEWS[r]
-  	end
+  	    @coords[-1] = NEWS[(NEWS.index(@coords[-1]) + 1) % NEWS.size]
       end
-      next
     end
     
     session[:coords] = @coords
